61a7aff86ec37f344c4e9361f5d599125af6f17a
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-08-25 04:19:01 +0530

    ChatWrapper

---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,7 @@
+const PdfRenderer=()=>{
+    return <div className="w- full bg-white rounded-md shadow flex fles-col item-center">
+       <div className="h-14 border-b border-zinc-200 flex items-center justify-betwee px-2"></div> 
+       <div className=" flex-item-center gap-1.5"></div>
+        Top bar </div>
+}
+export default PdfRenderer
